298317444560de11eb5ba44f132af3de885b8f8d
Asqar Arslanov <asqarslanov@gmail.com>
2025-04-03 16:41:29 +0300

    add frb

--- trails_mobile/lib/main.dart
+++ trails_mobile/lib/main.dart
@@ -1,62 +1,92 @@
-import 'package:flutter/material.dart';
+// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
 
-void main() {
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// bool validateEmail(String value) {
+//   return value.length >= 3;
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   String _inputValue = "";
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             TextField(
+//               decoration: const InputDecoration(border: OutlineInputBorder()),
+//               onChanged: (value) {
+//                 setState(() {
+//                   _inputValue = value;
+//                 });
+//               },
+//             ),
+//             Text(validateEmail(_inputValue).toString()),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+
+import 'package:flutter/material.dart';
+import 'package:trails_mobile/src/rust/api/simple.dart';
+import 'package:trails_mobile/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-bool validateEmail(String value) {
-  return value.length >= 3;
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _inputValue = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: const InputDecoration(border: OutlineInputBorder()),
-              onChanged: (value) {
-                setState(() {
-                  _inputValue = value;
-                });
-              },
-            ),
-            Text(validateEmail(_inputValue).toString()),
-          ],
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Text(
+            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
+          ),
         ),
       ),
     );
